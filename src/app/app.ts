40bb474898583160464abb678446dae339bfc84c
MariaNanys <maria.iwaszczenko@gmail.com>
2026-03-04 14:04:18 +0100

    Create main view with Login button and searching section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss', 
-  imports: [NavigationComponent]
+  imports: [NavigationComponent, RouterOutlet]
 })
 export class App {
   protected readonly title = signal('Librarius');
---
+++ src/app/main-content/main-content.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+  selector: 'main-content',
+  templateUrl: './main-content.html',
+  styleUrl: './main-content.scss'
+})
+export class MainContentComponent {
+
+}
