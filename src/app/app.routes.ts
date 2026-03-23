e6c0bd9a5b4333fa2d5837b7059c77f49e0eeadf
MariaNanys <maria.iwaszczenko@gmail.com>
2026-03-23 12:25:30 +0100

    Create component with successfully registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home';
 import { LoginComponent } from './login/login';
 import { RegistrationComponent } from './registration/registration';
 import { SearchComponent } from './search/search';
+import { RegisterSuccessComponent } from './register-succes/register_success';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegistrationComponent},
+    {path: 'register-success', component: RegisterSuccessComponent},
     {path: 'search', component: SearchComponent},
 ];
---
+++ src/app/register-succes/register_success.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-register_success',
+  templateUrl: './register_success.html',
+  styleUrl: './register_success.scss'
+})
+
+export class RegisterSuccessComponent {
+
+}
